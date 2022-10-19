af83d240f02501eaf2bc70f7dd6e29df52a56545
LiGuiye <1171116470@qq.com>
2022-10-19 15:28:23 -0500

    update module loading, data path, and args to compat with hpcc device

--- job_submission.sh
+++ job_submission.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=ATPK-4S       # create a short name for your job
+#SBATCH --job-name=ATPK-8W       # create a short name for your job
 #SBATCH --partition=quanah
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=36       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --array=0-19:1           # job array with index values
+#SBATCH --cpus-per-task=20       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --array=0-1:1           # job array with index values
 #%module load matlab
 
 
-matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/Documents/ATPK/\"),genpath(\"/home/guiyli/Documents/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(4, Solar, $SLURM_ARRAY_TASK_ID); exit"
-
-
+module load matlab
+matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(8, \"Wind\", $SLURM_ARRAY_TASK_ID, 2); exit"
