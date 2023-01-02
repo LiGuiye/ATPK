804970b6f804aac660b7761654b03ba697bb464c
LiGuiye <1171116470@qq.com>
2023-01-01 21:00:33 -0600

    update for different scale and starting size

--- job_submission.sh
+++ job_submission.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=ATPK-8W       # create a short name for your job
+#SBATCH --job-name=W4       # create a short name for your job
 #SBATCH --partition=quanah
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=20       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --array=0-1:1           # job array with index values
+#SBATCH --cpus-per-task=2       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --array=0-399:1           # job array with index values
 #%module load matlab
 
 
 module load matlab
-matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(8, \"Wind\", $SLURM_ARRAY_TASK_ID, 2); exit"
+
+# matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(128, 4, \"Solar\", $SLURM_ARRAY_TASK_ID, 400, \"_from128\"); exit"
+
+matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(128, 4, \"Wind\", $SLURM_ARRAY_TASK_ID, 400, \"_from128\"); exit"
+
+# matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(8, 5, \"Solar\", $SLURM_ARRAY_TASK_ID, 400, \"\"); exit"
+
+# matlab -nodisplay -r "addpath(genpath(\"/home/guiyli/MyTmp/ATPK/\"),genpath(\"/home/guiyli/MyTmp/npy-matlab/npy-matlab/\")); main_function_ATPK_Guiye(8, 50, \"Wind\", $SLURM_ARRAY_TASK_ID, 400, \"\"); exit"
